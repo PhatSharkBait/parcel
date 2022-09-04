123e652436b7fa23b0e0fba504fcec6c295ea560
Cameron Bass <Cameronjudd98@gmail.com>
2022-09-03 21:22:16 -0600

    Created 321Go! with coroutines

--- ColorMatchGame/Assets/Scripts/CoroutineBehaviour.cs
+++ ColorMatchGame/Assets/Scripts/CoroutineBehaviour.cs
@@ -3,21 +3,39 @@ using UnityEngine;
 using UnityEngine.Events;
 
 public class CoroutineBehaviour : MonoBehaviour {
-    public float delayTime;
-    public UnityEvent delayEvent;
+    public float delayTime, countDownTime = 1.0f;
+    public IntDataSO counterDown;
+    public UnityEvent delayEvent, beginRepeatEvent, repeatEvent, endRepeatEvent;
 
-    private WaitForSeconds _waitForSeconds;
+    private WaitForSeconds _waitForSecondsDelay;
+    private WaitForSeconds _waitForSecondsCountDown;
 
     private void Awake() {
-        _waitForSeconds = new WaitForSeconds(delayTime);
+        _waitForSecondsDelay = new WaitForSeconds(delayTime);
+        _waitForSecondsCountDown = new WaitForSeconds(countDownTime);
     }
 
     private IEnumerator AfterDelayEvent() {
-        yield return _waitForSeconds;
+        yield return _waitForSecondsDelay;
         delayEvent.Invoke();
     }
-
-    public void BeginCoroutine() {
+    
+    private IEnumerator BeginCountdown() {
+        beginRepeatEvent.Invoke();
+        while (counterDown.value > 0) {
+            repeatEvent.Invoke();
+            counterDown.value --;
+            yield return _waitForSecondsCountDown;
+        }
+        endRepeatEvent.Invoke();
+    }
+    
+    public void BeginDelayCoroutine() {
         StartCoroutine(AfterDelayEvent());
     }
+
+    public void BeginCountDownCoroutine() {
+        StartCoroutine(BeginCountdown());
+    }
+
 }
