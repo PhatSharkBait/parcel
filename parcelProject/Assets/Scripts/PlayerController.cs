c02895bae3e1a74a6700065b0176dc4c8ff68db3
Cameron Bass <cameronjudd98@gmail.com>
2022-09-22 12:07:12 -0600

    Fixed movement. Can't work with coroutine

--- parcelProject/Assets/Scripts/PlayerController.cs
+++ parcelProject/Assets/Scripts/PlayerController.cs
@@ -8,29 +8,14 @@ public class PlayerController : MonoBehaviour {
     public float speed = 6f;
     
     private CharacterController _characterController;
-    private WaitForFixedUpdate _waitForFixedUpdate;
     private Vector3 _moveDir;
-    public bool _canMove;
     private void Awake() {
         _characterController = GetComponent<CharacterController>();
-        _waitForFixedUpdate = new WaitForFixedUpdate();
     }
 
     private void OnMove(InputValue value) {
         var direction = value.Get<Vector2>();
         _moveDir = (new Vector3(direction.x, 0, direction.y)) * speed;
-
-        // if (_moveDir == Vector3.zero) {
-        //     _canMove = false;
-        // } else if (!_canMove) {
-        //     StartCoroutine(PerformMovement());
-        // }
-
-        if (!_canMove && _moveDir != Vector3.zero) {
-            StartCoroutine(PerformMovement());
-        } else if (_moveDir == Vector3.zero) {
-            _canMove = false;
-        }
     }
     
     private void OnFire() {
@@ -40,12 +25,8 @@ public class PlayerController : MonoBehaviour {
     private void OnMiddleClick() {
         Debug.Log("hey");
     }
-
-    private IEnumerator PerformMovement() {
-        _canMove = true;
-        while (_canMove) {
-            _characterController.Move(_moveDir*Time.deltaTime);
-            yield return _waitForFixedUpdate;
-        }
+    
+    private void Update() { 
+        _characterController.Move(_moveDir * Time.deltaTime);
     }
 }
