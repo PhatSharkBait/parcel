7ebff22e19a36c497b7dd7ca4a53f4b27855d6b6
Cameron Bass <Cameronjudd98@gmail.com>
2022-09-29 16:06:32 -0600

    Enemies follow player

--- parcelProject/Assets/Scripts/EnemyTypeBehaviour.cs
+++ parcelProject/Assets/Scripts/EnemyTypeBehaviour.cs
@@ -1,19 +1,33 @@
 using System;
 using UnityEngine;
+using UnityEngine.Events;
+using unityTools;
+using System.Collections;
+using UnityEditor;
 
 [RequireComponent(typeof(MeshRenderer))]
+[RequireComponent(typeof(Rigidbody))]
 public class EnemyTypeBehaviour : MonoBehaviour {
     public EnemySO enemySO;
     public EnemyPoolSO enemyPoolSO;
-    //public UnityEvent swapEnemy;
+    public Vector3DataSO playerLocationSO;
+    public int maxDistanceFromPlayer = 5;
+    public UnityEvent outOfRangeEvent;//, swapEnemy;
 
     private MeshRenderer _meshRenderer;
+    private Rigidbody _rigidbody;
+    private WaitForSeconds _waitForSeconds;
+    private float _speed;
+    private float _damage;
 
     private void Awake() {
         _meshRenderer = GetComponent<MeshRenderer>();
+        _rigidbody = GetComponent<Rigidbody>();
+        _waitForSeconds = new WaitForSeconds(.5f);
     }
 
     private void Start() {
+        StartCheckDistanceCoroutine();
         SwapEnemy();
     }
 
@@ -26,5 +40,31 @@ public class EnemyTypeBehaviour : MonoBehaviour {
     private void SwapEnemy() {
         enemySO = enemyPoolSO.enemyType;
         _meshRenderer.material = enemySO.material;
+        _damage = enemySO.damage;
+        _speed = enemySO.moveSpeed;
+    }
+    
+    public void StartCheckDistanceCoroutine() {
+        StartCoroutine(CheckDistance());
+    }
+
+    private IEnumerator CheckDistance() {
+        while (InRange()) {
+            yield return _waitForSeconds;
+        }
+        OutOfRange();
+    }
+
+    private bool InRange() {
+        return maxDistanceFromPlayer > (playerLocationSO.value - transform.position).magnitude;
+    }
+
+    private void OutOfRange() {
+        outOfRangeEvent.Invoke();
+    }
+
+    private void Update() {
+        Vector3 moveDir = (playerLocationSO.value - transform.position).normalized * (_speed * Time.deltaTime);
+        transform.Translate(moveDir);
     }
 }
